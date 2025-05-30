dfa0b11e6b0f89b546e72be4cf4b0d5bff888286
Bassirou LOUM <bassirou825@gmail.com>
2025-05-30 17:44:59 +0000

    first commit

---
+++ docs-administ/src/main/java/sn/esp/ipld/docs_administ/DocsAdministApplication.java
@@ -0,0 +1,13 @@
+package sn.esp.ipld.docs_administ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocsAdministApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocsAdministApplication.class, args);
+	}
+
+}
