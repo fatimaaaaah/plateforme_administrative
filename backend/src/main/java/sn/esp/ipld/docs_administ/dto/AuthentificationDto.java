e8e6ed9898fd0f7a6287921ffdeca494f88ef482
mously-diakhate <mouslydiakhate1@gmail.com>
2025-07-01 14:34:45 +0000

    separation du backend

--- backend/src/main/java/sn/esp/ipld/docs_administ/dto/AuthentificationDto.java
+++
@@ -1,5 +0,0 @@
-package sn.esp.ipld.docs_administ.dto;
-
-public record AuthentificationDto(String username,String password) {
-}
-
--- backend/src/main/java/sn/esp/ipld/docs_administ/enumeration/TypeRole.java
+++
@@ -1,8 +0,0 @@
-package sn.esp.ipld.docs_administ.enumeration;
-
-public enum TypeRole {
-  UTILISATEUR,
-  ADMINISTRATEUR,
-  AGENTADMINIST,
-  CHEF_QUARTIER
-}
